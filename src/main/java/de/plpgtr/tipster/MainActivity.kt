a3d53384f5a5f34991454d39a25474aa918dfd45
philipp.gaechter <philipp.gaechter@libify.com>
2023-03-25 15:04:01 +0100

    init

---
+++ src/main/java/de/plpgtr/tipster/MainActivity.kt
@@ -0,0 +1,13 @@
+package de.plpgtr.tipster
+
+import android.R
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
