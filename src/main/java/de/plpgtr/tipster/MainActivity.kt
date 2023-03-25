8e1845e07fa981d6cc47ab592dac99de8e68ab20
philipp.gaechter <philipp.gaechter@libify.com>
2023-03-25 15:45:30 +0100

    Added not working slider

--- src/main/java/de/plpgtr/tipster/MainActivity.kt
+++ src/main/java/de/plpgtr/tipster/MainActivity.kt
@@ -2,6 +2,7 @@ package de.plpgtr.tipster
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.Slider
 import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val percentageSlider = findViewById<Slider>(R.id.percentage_slider)
+        val percentage = percentageSlider.values[0]
     }
+
 }
